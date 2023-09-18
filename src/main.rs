9cbaa299b9a41b69efbb3c8df977fa0e62e0ec44
taintedabomination <taintedabomination@proton.me>
2023-09-18 09:40:15 +0200

    Making HTTP requests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
+use reqwest;
 use std::collections::HashMap;
 use std::env;
+use tokio;
+
+async fn test_fetch() -> Result<(), reqwest::Error> {
+    let url = "https://jsonplaceholder.typicode.com/posts/1";
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+
+    println!("Odpověď: {}", body);
+
+    Ok(())
+}
 
 fn print_timetable() -> () {
     println!("Rozvrh");
 }
 
 fn print_marks() -> () {
-    println!("Rozvrh");
+    println!("Známky");
 }
 
 fn print_changes() -> () {
     println!("Změny v rozvrhu");
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
     let total_args = args.len();
+
+    let _ = test_fetch().await;
 
     let mut command_map: HashMap<&str, Box<dyn Fn()>> = HashMap::new();
     command_map.insert("rozvrh", Box::new(print_timetable));
