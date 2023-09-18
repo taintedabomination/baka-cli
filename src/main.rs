dee4bf0cddf8a7c3f58924d6cddd5a23473911fa
taintedabomination <taintedabomination@proton.me>
2023-09-18 06:20:19 +0200

    Reading the program's arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let total_args = args.len();
+
+    if total_args <= 1 {
+        eprintln!("Použití: baka-cli <prikaz>");
+        std::process::exit(1);
+    }
+
+    println!("Použitý příkaz: {}", args[1]);
 }
