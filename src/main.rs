aab007d847880c3e0b356b738f50321184e64e39
taintedabomination <taintedabomination@proton.me>
2023-09-18 06:31:04 +0200

    Running a function based on the command argument

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
+use std::collections::HashMap;
 use std::env;
+
+fn print_timetable() -> () {
+    println!("Rozvrh");
+}
+
+fn print_marks() -> () {
+    println!("Rozvrh");
+}
+
+fn print_changes() -> () {
+    println!("Změny v rozvrhu");
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let total_args = args.len();
+
+    let mut command_map: HashMap<&str, Box<dyn Fn()>> = HashMap::new();
+    command_map.insert("rozvrh", Box::new(print_timetable));
+    command_map.insert("znamky", Box::new(print_marks));
+    command_map.insert("zmeny", Box::new(print_changes));
 
     if total_args <= 1 {
         eprintln!("Použití: baka-cli <prikaz>");
         std::process::exit(1);
     }
 
-    println!("Použitý příkaz: {}", args[1]);
+    let command = &args[1];
+    match command_map.get(command.as_str()) {
+        Some(func) => func(),
+        None => {
+            eprintln!("Neznámý příkaz: {}!", command);
+            std::process::exit(1);
+        }
+    }
 }
